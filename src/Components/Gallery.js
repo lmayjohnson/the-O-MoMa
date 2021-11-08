64010afcbe1a2ff6d4523e6fa5bcc67228981172
lmayjohnson <lmayjohnson.94@gmail.com>
2021-11-07 21:29:11 -0800

    Added modals for gallery. TODO: figure out how to access JSON Data outside of req.onreadystatechange func.

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Gallery.css";
 import { Card } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
 import img1 from "../imgs/POP SHOW.png";
 import img2 from "../imgs/self portrait.png";
 import img3 from "../imgs/at the sursock museum.png";
@@ -11,16 +10,27 @@ import img6 from "../imgs/untitled.png";
 import img7 from "../imgs/947-5.png";
 import img8 from "../imgs/diego on my mind.png";
 import img9 from "../imgs/the virgins.png";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
 
 import { Container, Row, Col } from "react-grid-system";
 
 const Gallery = () => {
   let req = new XMLHttpRequest();
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  var resultAsObj;
   req.onreadystatechange = () => {
     if (req.readyState === XMLHttpRequest.DONE) {
-      const resultAsObj = JSON.parse(req.response);
+      resultAsObj = JSON.parse(req.response);
       console.log({ resultAsObj });
+      console.log(
+        " ++++ ARTIST NAME IS:",
+        resultAsObj.artist_biographies[0].name
+      );
     }
   };
 
@@ -29,43 +39,81 @@ const Gallery = () => {
     "x-master-key",
     "$2b$10$s7H4sIMBHHg90w2OHbw.WOabecMFwItIyyY9Oi3HxZV5ANKnwcGo6"
   );
+
   req.send();
 
   return (
     <Container className="container-style">
       <Row>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Painting Name {}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Bio: {}</Modal.Body>
+          <Modal.Footer></Modal.Footer>
+        </Modal>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img1} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img1}
+            onClick={handleShow}
+          />
         </Col>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img2} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img2}
+            onClick={handleShow}
+          />
         </Col>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img3} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img3}
+            onClick={handleShow}
+          />
         </Col>
       </Row>
       <br /> <br />
       <Row>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img4} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img4}
+            onClick={handleShow}
+          />
         </Col>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img5} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img5}
+            onClick={handleShow}
+          />
         </Col>
         <Col sm={4}>
-          <Card.Img classname="img-sizing" variant="top" src={img6} />
+          <Card.Img
+            classname="img-sizing"
+            variant="top"
+            src={img6}
+            onClick={handleShow}
+          />
         </Col>
       </Row>
       <br /> <br />
       <Row>
         <Col sm={4}>
-          <Card.Img variant="top" src={img7} />
+          <Card.Img variant="top" src={img7} onClick={handleShow} />
         </Col>
         <Col sm={4}>
-          <Card.Img variant="top" src={img8} />
+          <Card.Img variant="top" src={img8} onClick={handleShow} />
         </Col>
         <Col sm={4}>
-          <Card.Img variant="top" src={img9} />
+          <Card.Img variant="top" src={img9} onClick={handleShow} />
         </Col>
       </Row>
     </Container>
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -8,6 +8,7 @@ const Map = () => {
     <Card className="Cardex" style={{ width: "20rem" }}>
       <Card.Body>
         <iframe
+          title="Where are we located?"
           className="Map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199412.48571308967!2d-90.38354701067739!3d38.65301691773768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d8b4a9faed8ef9%3A0xbe39eaca22bbe05b!2sSt.%20Louis%2C%20MO!5e0!3m2!1sen!2sus!4v1636316812629!5m2!1sen!2sus"
         ></iframe>
